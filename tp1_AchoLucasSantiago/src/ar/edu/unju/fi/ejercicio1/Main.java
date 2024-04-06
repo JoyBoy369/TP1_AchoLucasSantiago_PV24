c0b63dc6093d1618cd699398579c46fafe3f6529
Lucas <Lucas@DESKTOP-T7HV4G3>
2024-04-05 21:06:26 -0300

    Creacion de la estructura general y subida del Ejercicio1

---
+++ tp1_AchoLucasSantiago/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,27 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		// Declarar las variables
+        int num1, num2, num3, num4, num5;
+        double promedio;
+        
+        // Asignar valores numéricos a cada variable
+        num1 = 34;
+        num2 = 2;
+        num3 = 24;
+        num4 = 12;
+        num5 = 5;
+        
+        // Calcular el promedio
+        promedio = (num1 + num2 + num3 + num4 + num5) / 5.00;
+        
+        // Mostrar el resultado por consola
+        System.out.println("El promedio es: " + promedio);
+		
+	}
+
+}
