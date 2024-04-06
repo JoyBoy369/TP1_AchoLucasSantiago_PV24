05bef17d6aea28218cf65088b229ab4fbd6087e0
LucasA <Lucas@DESKTOP-T7HV4G3>
2024-04-05 23:36:41 -0300

    resolucion del Ejercicio2

--- tp1_AchoLucasSantiago/src/ar/edu/unju/fi/ejercicio2/Main.java
+++ tp1_AchoLucasSantiago/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -3,7 +3,22 @@ package ar.edu.unju.fi.ejercicio2;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		// Declaración de variables
+        String pais = "España";
+        int edad = 30;
+        double alturaEdificio = 150.5; 
+        double precioProducto = 10.99; 
+        String telefono = "+54 388653274";
+        double coseno = Math.cos(0.5);
+        
+        // Mostrando los valores por consola
+        System.out.println("Nombre del país: " + pais);
+        System.out.println("Edad de la persona: " + edad + " años");
+        System.out.println("Altura del edificio: " + alturaEdificio + " metros");
+        System.out.println("Precio del producto: $" + precioProducto);
+        System.out.println("Número de teléfono: " + telefono);
+        System.out.println("Coseno de 0.5: " + coseno);
 
 	}
 
